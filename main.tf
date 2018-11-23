da06ae4ba4ef6b48b578371af3d320af67711da2
Max G <maxdotdotg@gmail.com>
2018-11-23 17:07:56 -0500

    re-order resources to have ec2 instance with deps as first resource, and dependant resources following, add second ec2 instance resource

--- main.tf
+++ main.tf
@@ -2,10 +2,13 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "example" {
-    bucket  = "mg_tftst_bucket"
-    acl     = "private"
-    # at a later point, review how policies work with terraform
+resource "aws_instance" "node1" {
+    ami             = "ami-b374d5a5"
+    instance_type   = "t2.micro"
+    depends_on      = ["aws_s3_bucket.example"]
+    # explicit dependency declaration, just like dependsOn directive
+    # from cloudformation
+    # terraform cannot resolve this bit by itself, thus the use of depends_on
 }
 
 resource "aws_eip" "ip" {
@@ -14,11 +17,13 @@ resource "aws_eip" "ip" {
     # terraform can resolve this bit by itself
 }
 
-resource "aws_instance" "node1" {
+resource "aws_s3_bucket" "example" {
+    bucket  = "mg_tftst_bucket"
+    acl     = "private"
+    # at a later point, review how policies work with terraform
+}
+
+resource "aws_instance" "node2" {
     ami             = "ami-b374d5a5"
     instance_type   = "t2.micro"
-    depends_on      = ["aws_s3_bucket.example"]
-    # explicit dependency declaration, just like dependsOn directive
-    # from cloudformation
-    # terraform cannot resolve this bit by itself, thus the use of depends_on
 }
