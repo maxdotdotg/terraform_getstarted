e8f869ac46f2f2a7029d4aee5ad855f31b8d395e
Max G <maxdotdotg@gmail.com>
2018-11-23 17:02:07 -0500

    add s3 dep to main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,23 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "example" {
+    bucket  = "mg_tftst_bucket"
+    acl     = "private"
+    # at a later point, review how policies work with terraform
+}
+
 resource "aws_eip" "ip" {
     instance = "${aws_instance.node1.id}"
+    # implicit depenency to ec2
+    # terraform can resolve this bit by itself
 }
 
 resource "aws_instance" "node1" {
     ami             = "ami-b374d5a5"
     instance_type   = "t2.micro"
+    depends_on      = ["aws_s3_bucket.example"]
+    # explicit dependency declaration, just like dependsOn directive
+    # from cloudformation
+    # terraform cannot resolve this bit by itself, thus the use of depends_on
 }
