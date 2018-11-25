5fff47b0b49384a98fce6517013c51222c1f37c2
Max G <maxdotdotg@gmail.com>
2018-11-24 19:13:30 -0500

    clear out main.tf and replace with consul module example

--- main.tf
+++ main.tf
@@ -2,27 +2,12 @@ provider "aws" {
     region    = "${var.region}"
 }
 
-resource "aws_instance" "node1" {
-    ami             = "${var.ami_id}"
-    instance_type   = "t2.micro"
-    key_name        = "${var.key_name}"
+module "consul" {
+  source ="hashicorp/consul/aws"
 
-    provisioner "local-exec" {
-      command = "echo ${aws_instance.node1.public_ip} > ip_address.txt"
-    }
-    # provioners only executed on resource creation
+  num_servers = "3"
 }
 
-resource "aws_eip" "ip" {
-    instance = "${aws_instance.node1.id}"
-    # implicit depenency to ec2
-    # terraform can resolve this bit by itself
-}
-
-output "ip" {
-  value = "${aws_eip.ip.public_ip}"
-}
-
-output "private_dns" {
-  value = "${aws_instance.node1.private_dns}"
+output "consul_server_asg_name" {
+  value = "${module.consul.asg_name_servers}"
 }
